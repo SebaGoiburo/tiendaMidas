d8b4ddb5f9fda8cb4a4c5c9d530924e880a49e39
Sebastian Goiburo <sebastiangoiburo@gmail.com>
2023-09-26 23:08:24 -0300

    Primer Commit

---
+++ src/main/java/com/tiendaMidas/tiendaMidas/TiendaMidasApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaMidas.tiendaMidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaMidas/tiendaMidas/TiendaMidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaMidas.tiendaMidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
